9e53386cf62b7f30297c0792eb19777d0f20202a
patchzyy <1055156@hr.nl>
2023-03-17 12:44:00 +0100

    ff test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// hierin het main programma?
+static class Program{
+    static void Main(){
+            Console.WriteLine("Hello and welcome to rotterdam airport!!");
+            
+
+    }
+
+}
