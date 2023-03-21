68f81e6a380fcc405e3aa105aaa9fa5cd400f8a2
patchzyy <1055156@hr.nl>
2023-03-21 17:24:35 +0100

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -10,12 +10,11 @@ class Menu{
         Console.WriteLine("Welkom bij het main menu");
         Console.ResetColor();
         Console.WriteLine("gebruik pijltje omhoog en omlaag om door het menu te scrollen");
-        string kleur = "--> \u001b[32m";
+        string kleur = "-->  \u001b[32m";
         (int left, int top) = Console.GetCursorPosition();
         while(selected == false){
         Console.Clear();
         Console.SetCursorPosition(left, top);
-        Console.WriteLine(option);
         Console.WriteLine($"{(option == 0 ? kleur : "    ")}   Login\u001b[0m");
         Console.WriteLine($"{(option == 1 ? kleur : "    ")}   optie 2\u001b[0m");
         Console.WriteLine($"{(option == 2 ? kleur : "    ")}   optie 3\u001b[0m");
