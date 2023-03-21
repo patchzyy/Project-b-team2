0c6c64e9f3a6d232d32f0de16812ab89b2e1cded
patchzyy <1055156@hr.nl>
2023-03-21 16:18:16 +0100

    main menu nog niet af

--- Menu.cs
+++ Menu.cs
@@ -1,21 +1,38 @@
 class Menu{
     Boolean selected = false;
     ConsoleKeyInfo key;
+    int option = 1;
+    
+
+
     public Menu(){
 
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("Welkom bij het main menu");
         Console.ResetColor();
         Console.WriteLine("gebruik pijltje omhoog en omlaag om door het menu te scrollen");
-
+        string kleur = "--> \u001b[32m";
+        (int left, int top) = Console.GetCursorPosition();
         while(selected == false){
+        Console.SetCursorPosition(left, top);
+        Console.WriteLine($"{(option == 1 ? kleur : "    ")}   Login\u001b[0m");
+        Console.WriteLine($"{(option == 2 ? kleur : "    ")}   optie 2\u001b[0m");
+        Console.WriteLine($"{(option == 3 ? kleur : "    ")}   optie 3\u001b[0m");
+        Console.WriteLine($"{(option == 4 ? kleur : "    ")}   optie 4\u001b[0m");
+        key = Console.ReadKey(true);
 
-        
-        Console.WriteLine("    \nLogin");
-        Console.WriteLine("--> \u001b[32moptie 2\u001b[0m");
-        Console.WriteLine("   optie 3");
-        Console.WriteLine("   optie 4");
-        var key = Console.ReadKey(true);
+        switch(key.Key)
+        {
+            case ConsoleKey.DownArrow:
+                option++;
+                break;
+            case ConsoleKey.UpArrow:
+                option--;
+                break;
+            case ConsoleKey.Enter:
+                selected = true;
+                break;
+        }
         }
     }
 }
