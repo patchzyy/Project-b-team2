5fea960561a67962037368218f157b9d0da5db7e
MelihBayrakdar <1056209@hr.nl>
2023-03-27 09:10:04 +0200

    2 methods toegevoegd aan Login class

--- Login.cs
+++ Login.cs
@@ -2,6 +2,18 @@ class Login : User
 {
     public Login(string name, string email, string password, bool has_admin) : base(name, email, password, has_admin)
     {
-        
+
+    }
+
+    public User LoggingIn(string email, string password)
+    {
+        // if CheckDatabase is true
+        // create the designated User object
+        // return User object
+    }
+
+    private bool CheckDatabase(string email, string password)
+    {
+        // return account found in database
     }
 }
