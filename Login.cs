0644de7ff9039addbe576485632d4d995d244e12
MelihBayrakdar <1056209@hr.nl>
2023-03-28 17:32:15 +0200

    Login class helemaal voltooid
    De login class is voltooid. De gebruiker kan een email inzetten, waarna deze email in de database wordt nagecheckt. Hier en daar zijn de Expection throws gefixt, dus je wordt als gebruiker omgeleid bij het verkeerd geven van inputs.
    
    Er is gebruik gemaakt van zowel public als private methods. Als je even niet meer weet hoe die twee te implementeren, maak gerust gebruik van de voorbeeld van Login class. Het belangrijkste van allemaal is dat je er voor zorgt niet meer dan 3 keer te intenden.
    
    De taak wordt verplaatst naar de review sectie vanaf de test sectie in Trello. Mogelijke veranderingen kunnen voortkomen over de UI van de tekst, manier van input vragen en eventueel logica veranderingen.
    
    Voor nu is de class meerdere malen getest en is nu in werkende staat.

--- Login.cs
+++ Login.cs
@@ -1,19 +1,127 @@
-class Login : User
-{
-    public Login(string name, string email, string password, bool has_admin) : base(name, email, password, has_admin)
-    {
+using Microsoft.Data.Sqlite;
 
+class Login
+{
+    private static SqliteConnection connection = new SqliteConnection("Data Source=airline_data.db");
+    public static void LoggingIn()
+    {
+        Console.WriteLine("SIGN IN\n\n\n");
+
+        string email = AskForEmail();
+
+        // string userInfo = GetUser(email);
+        string userInfo;
+        while (true)
+        {
+            userInfo = GetUser(email);
+            if (userInfo != "")
+            {
+                break;
+            }
+            email = AskForEmail();
+        }
+
+        string userName = userInfo.Split(",")[0];
+        string correctPassword = userInfo.Split(",")[2];
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Password: ");
+                string password = Console.ReadLine();
+                if (correctPassword == password)
+                {
+                    // wat je ook doet, laat deze stukje aan niemand zien aub
+                    // HIGH SECRET
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\nLogging in.");
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\nLogging in..");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                    Console.WriteLine("\n\n\n\nLogging in...");
+                    Thread.Sleep(190);
+                    Console.WriteLine($"Logging succes, welcome {userName}.");
+                    break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter the correct format.");
+            }
+        }
     }
 
-    // public User LoggingIn(string email, string password)
-    // {
-    //     // if CheckDatabase is true
-    //     // create the designated User object
-    //     // return User object
-    // }
+    private static string AskForEmail()
+    {
+        string? email;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Email: ");
+                email = Console.ReadLine();
+                if (!email.Contains("@"))
+                {
+                    Console.WriteLine("Your email must have a '@'.");
+                    continue;
+                }
+                if (!email.Contains("."))
+                {
+                    Console.WriteLine("Your email must declare a domain name system.");
+                    continue;
+                }
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter the correct format.");
+            }
+        }
+        return email;
+    }
 
-    // private bool CheckDatabase(string email, string password)
-    // {
-    //     // return account found in database
-    // }
+    private static string GetUser(string email)
+    {
+        connection.Open();
+
+        string sql = "select * from [Users] where [email] = @email";
+        SqliteCommand command = default;
+        try
+        {
+            command = new SqliteCommand(sql, connection);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Wrong run error");
+        }
+
+        // value toevoegen aan de @email
+        command.Parameters.AddWithValue("@email", email);
+
+        // om sql queries te kunnen lezen gebruik je Reader
+        SqliteDataReader reader = command.ExecuteReader();
+        string userInfo = "";
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                string nameValue = reader.GetString(1);
+                string emailValue = reader.GetString(2);
+                string passwordValue = reader.GetString(3);
+                userInfo = $"{nameValue},{emailValue},{passwordValue}";
+            }
+        }
+        else
+        {
+            Console.WriteLine("The email has not been found. Try again.");
+            // string emailtryagain = AskForEmail();
+            // GetUser(emailtryagain);
+            return userInfo;
+        }
+        return userInfo;
+    }
 }
