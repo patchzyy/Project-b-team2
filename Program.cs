332ff29ccccaf59a2ed7b5ef01a484fe69fa1226
RowinPelk <1053763@hr.nl>
2023-03-28 13:25:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,47 +5,7 @@
     {
         Information.DisplayLogo();
         Thread.Sleep(2000);
-        User currentuser = null;
-        if (currentuser == null)
-        {
-            Menu mainmenu = new Menu(new List<string>() { "Login", "Registreer", "Informatie", "Boeken" });
-            int selectedOption = mainmenu.ShowMenu();
-            // 1: Login, 2: Registreer, 3: Informatie, 4: Boeken
-            switch (selectedOption)
-            {
-                case 1:
-                    // Login code
-                    break;
-                case 2:
-                    // Registreer code
-                    break;
-                case 3:
-                    // Informatie code
-                    break;
-                case 4:
-                    // Boeken code
-                    break;
-
-
-            }
-        }
-        else
-        {
-            Menu usermenu = new Menu(new List<string>() { "Informatie", "Boeken", "Logout" });
-            int selectedOption = usermenu.ShowMenu();
-            switch (selectedOption)
-            {
-                case 1:
-                    // Informatie code
-                    break;
-                case 2:
-                    // Boeken code
-                    break;
-                case 3:
-                    // Logout code
-                    break;
-            }
-        }
+        Menu.Run();
     }
 
 
